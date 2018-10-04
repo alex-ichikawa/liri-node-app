0cf6a0c61da389931ff1fa9d227931224b84cd0c
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-10-03 21:47:24 -0700

    append file

--- liri.js
+++ liri.js
@@ -62,7 +62,8 @@ function concertThis() {
     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(err, response, body) {
         if (!err && response.statusCode === 200) {
             let date = moment(JSON.parse(body)[0].datetime).format("L");
-            console.log(`\r\nVenue: ${JSON.parse(body)[0].venue.name}\r\nState: ${JSON.parse(body)[0].venue.region}\r\nCity: ${JSON.parse(body)[0].venue.city}\r\nDate: ${date}`);
+            console.log(`\r\nArtist: ${userRequest}\r\nVenue: ${JSON.parse(body)[0].venue.name}\r\nState: ${JSON.parse(body)[0].venue.region}\r\nCity: ${JSON.parse(body)[0].venue.city}\r\nDate: ${date}`);
+            fs.appendFile("log.txt", `\r\nVenue: ${JSON.parse(body)[0].venue.name}\r\nState: ${JSON.parse(body)[0].venue.region}\r\nCity: ${JSON.parse(body)[0].venue.city}\r\nDate: ${date}`);
         } else {
             console.log(err);
         }
@@ -75,6 +76,7 @@ function spotifyThisSong() {
         type: 'track', query: song,
     }).then(function(data) {
         console.log(`\r\nArtist: ${data.tracks.items[0].artists[0].name}\r\nTitle: ${userRequest}\r\nAlbum: ${data.tracks.items[0].name}\r\nLink: ${data.tracks.items[0].artists[0].external_urls.spotify}`);
+        fs.appendFile("log.txt", `\r\nArtist: ${data.tracks.items[0].artists[0].name}\r\nTitle: ${userRequest}\r\nAlbum: ${data.tracks.items[0].name}\r\nLink: ${data.tracks.items[0].artists[0].external_urls.spotify}`);
     })
         .catch(function(err) {
             console.log(`\r\nArtist: Ace of Base\r\nTitle: The Sign\r\nAlbum: The Sign\r\nLink: https://open.spotify.com/album/5UwIyIyFzkM7wKeGtRJPgB`);
@@ -86,6 +88,7 @@ function movieThis() {
     request("http://www.omdbapi.com/?apikey=trilogy&t=" + movie, function(err, response, body) {
         if (!err && response.statusCode === 200) {
             console.log(`\r\nTitle: ${JSON.parse(body).Title}\r\nTitle: ${JSON.parse(body).Year}\r\nIMBD Rating: ${JSON.parse(body).imdbRating}\r\nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}\r\nProduced In: ${JSON.parse(body).Country}\r\nLanguage: ${JSON.parse(body).Language}\r\nPlot: ${JSON.parse(body).Plot}\r\nActors: ${JSON.parse(body).Actors}`);
+            fs.appendFile("log.txt",`\r\nTitle: ${JSON.parse(body).Title}\r\nTitle: ${JSON.parse(body).Year}\r\nIMBD Rating: ${JSON.parse(body).imdbRating}\r\nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}\r\nProduced In: ${JSON.parse(body).Country}\r\nLanguage: ${JSON.parse(body).Language}\r\nPlot: ${JSON.parse(body).Plot}\r\nActors: ${JSON.parse(body).Actors}` )
         }
     });
 }
